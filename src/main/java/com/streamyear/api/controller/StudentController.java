457114921c4da642e31698e511cfb6c6f6663840
StreamYear <2500931932@qq.com>
2018-09-13 10:40:54 +0800

    搭建框架。

--- src/main/java/com/streamyear/api/controller/StudentController.java
+++ src/main/java/com/streamyear/api/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.streamyear.api.controller;
 
 import com.streamyear.api.pojo.Student;
 import com.streamyear.api.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/student")
 public class StudentController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     private StudentService studentService;
 
@@ -18,5 +22,12 @@ public class StudentController {
     public List<Student> listStudentByName(){
         List<Student> result = studentService.listStudentByName("小王");
         return result;
+    }
+
+    @RequestMapping("log")
+    public void logTest(){
+        LOGGER.info("info消息!!!");
+        LOGGER.debug("debug消息!!!");
+        LOGGER.error("error消息!!!");
     }
 }
