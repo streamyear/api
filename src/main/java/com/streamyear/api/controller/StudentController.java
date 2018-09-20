e44ec291243d4d50a3417d9dc1121dcf2746bfad
StreamYear <2500931932@qq.com>
2018-09-20 17:11:20 +0800

    集成rocketmq

--- src/main/java/com/streamyear/api/controller/StudentController.java
+++ src/main/java/com/streamyear/api/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.streamyear.api.controller;
 
+import com.streamyear.api.consumer.DemoMqContent;
 import com.streamyear.api.pojo.Student;
 import com.streamyear.api.pojo.User;
 import com.streamyear.api.service.StudentService;
+import io.github.rhwayfun.springboot.rocketmq.starter.common.DefaultRocketMqProducer;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,9 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @Autowired
+    private DefaultRocketMqProducer producer;
 
     @RequestMapping("listStudentByName")
     public List<Student> listStudentByName(){
@@ -36,5 +43,15 @@ public class StudentController {
         LOGGER.info("info消息!!!");
         LOGGER.debug("debug消息!!!");
         LOGGER.error("error消息!!!");
+    }
+
+    @RequestMapping("mq")
+    public void mqTest(){
+        DemoMqContent content = new DemoMqContent();
+        content.setId(1);
+        content.setDesc("您好啊!");
+        Message msg = new Message("test-topic","test-tag",content.toString().getBytes());
+        boolean b = producer.sendMsg(msg);
+        LOGGER.info("发送结果: " + b);
     }
 }
